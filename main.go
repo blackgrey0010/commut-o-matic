21529c208fa209cb3763fa3bee86f4166cf915e1
Karissa <karissapeth@hotmail.com>
2018-09-30 09:54:02 -0700

    started struct of driving mode request

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps" // https://godoc.org/googlemaps.github.io/maps
 )
-func main(){
-    fmt.Println("Hello world")
+
+func main() {
+	apiKey := os.Getenv("GCP_API_KEY")
+
+	if apiKey == "" {
+		log.Fatalf("GCP API key not set")
+
+	}
+
+	c, err := maps.NewClient(maps.WithAPIKey(apiKey))
+	if err != nil {
+		log.Fatalf("API Client not created: %s", err)
+	}
+	r := &maps.DistanceMatrixRequest{
+		Origins:       []string{"647 Central Ave, Alameda 94501"},
+		Destinations:  []string{"61 Oak Grove St, San Francisco 94107"},
+		DepartureTime: `now`,
+		Mode:          `ModeDriving`,
+	}
+	resp, err := c.DistanceMatrix(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	fmt.Println(resp)
+	fmt.Println("")
+	fmt.Println("Hello boobaby! Here is your commute:")
 }
