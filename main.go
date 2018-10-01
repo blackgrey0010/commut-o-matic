b63681ac04df563441a88d205d1185f8b1115498
Christian Peth <sspectre99@gmail.com>
2018-10-01 16:55:05 -0700

    get responses for both driving and transit, output both results and pick a winner

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ import (
 	"googlemaps.github.io/maps" // https://godoc.org/googlemaps.github.io/maps
 )
 
+const (
+	Origin      = "647 Central Ave, Alameda 94501"
+	Destination = "61 Oak Grove St, San Francisco 94107"
+)
+
 func main() {
 	apiKey := os.Getenv("GCP_API_KEY")
 
@@ -17,22 +22,46 @@ func main() {
 
 	}
 
-	c, err := maps.NewClient(maps.WithAPIKey(apiKey))
+	client, err := maps.NewClient(maps.WithAPIKey(apiKey))
 	if err != nil {
 		log.Fatalf("API Client not created: %s", err)
 	}
-	r := &maps.DistanceMatrixRequest{
-		Origins:       []string{"647 Central Ave, Alameda 94501"},
-		Destinations:  []string{"61 Oak Grove St, San Francisco 94107"},
-		DepartureTime: `now`,
-		Mode:          `ModeDriving`,
+
+	driving := getTravelTimes(maps.TravelModeDriving, client)
+	transit := getTravelTimes(maps.TravelModeTransit, client)
+
+	outputResponse(driving)
+	outputResponse(transit)
+
+	quickestMode := "Driving"
+	if driving.Rows[0].Elements[0].Duration > transit.Rows[0].Elements[0].Duration {
+		quickestMode = "Transit"
 	}
-	resp, err := c.DistanceMatrix(context.Background(), r)
+
+	fmt.Println("")
+	fmt.Printf("Hello boobaby! Here is your commute: %s!\n", quickestMode)
+}
+
+func getTravelTimes(mode maps.Mode, c *maps.Client) *maps.DistanceMatrixResponse {
+	drivingReq := &maps.DistanceMatrixRequest{
+		Origins:       []string{Origin},
+		Destinations:  []string{Destination},
+		DepartureTime: `now`,
+		Mode:          mode,
+	}
+
+	resp, err := c.DistanceMatrix(context.Background(), drivingReq)
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
 
-	fmt.Println(resp)
-	fmt.Println("")
-	fmt.Println("Hello boobaby! Here is your commute:")
+	return resp
+}
+
+func outputResponse(resp *maps.DistanceMatrixResponse) {
+	for _, row := range resp.Rows {
+		for _, element := range row.Elements {
+			fmt.Printf("Distance:%s Duration:%s InTraffic:%s\n", element.Distance.HumanReadable, element.Duration, element.DurationInTraffic)
+		}
+	}
 }
